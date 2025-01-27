08c30edcaecae2420658b88bcca3d6a9322c36e5
Yu-Hyungi <lyou000000@naver.com>
2025-01-27 22:51:37 +0900

    Add files via upload

--- indexJs.js
+++ indexJs.js
@@ -9,23 +9,36 @@ function input(){
         for(var i =0;i<20;i++){
             input[i].style.borderColor='rgb(107, 107, 116)';
             input[i].style.backgroundColor='white';
+            input[i].value = "";
         }
         return;
     }
+    var count = 0;
     for(var i=(times*5);i<5+(times*5);i++){
         const input = document.querySelectorAll(".input");
         if(correct[i%5]===input[i].value){
             input[i].style.borderColor='rgb(67, 253, 98)';
             input[i].style.backgroundColor='rgb(67, 253, 98)';
+            count++;
         }
         else{
-            input[i].style.borderColor='rgb(255, 87, 87)';
-            input[i].style.backgroundColor='rgb(255, 87, 87)';
+            for(var z=0;z<5;z++){
+                if(correct[z]===input[i].value){
+                    input[i].style.borderColor='rgb(229, 255, 80)';
+                    input[i].style.backgroundColor='rgb(229, 255, 80)';
+                    break;
+                }
+                else{
+                    input[i].style.borderColor='rgb(255, 87, 87)';
+                    input[i].style.backgroundColor='rgb(255, 87, 87)';
+                }
+            }
+            
         }
-        correct.map((value)=>{if(input[i]===value){
-            input[i].style.borderColor='rgb(229, 255, 80)';
-            input[i].style.backgroundColor='rgb(229, 255, 80)';
-        }});
+        if(count===5){
+            alert("성공!");
+            count = 0;
+        }
     }
     times++;
 }
